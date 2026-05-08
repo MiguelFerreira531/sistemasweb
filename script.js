f1cc49e68bb10e421c66b0dce7f32abdc223ea16
MiguelFerreira531 <ferreira.miguel@aluno.ifsp.edu.br>
2026-05-08 13:50:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function alterarFundo(){
-    document.body.style.backgroundColor = "red";
-
-}
+const formulario = document.getElementById('form');
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    const PnomeD = document.getElementById('pnome').value;
+    const UnomeD = document.getElementById('unome').value;
+    
+    const novo = {
+        pnome: pnomeD,
+        unome: unomeD
+    };
+});
+    
