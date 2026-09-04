6a88b9670f0f5bbc62bad666fb813e5bb47e2a09
MiguelFerreira531 <ferreira.miguel@aluno.ifsp.edu.br>
2026-09-04 14:35:22 -0300

    Corrige API, fetch e adiciona novo item à lista

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-let lista = [{titulo: 'titulo ficticio 1', descricao: 'obra ficticia'},
-{nome: 'nome do novo produto', descricao: 'para que serve o novo produto'}];
+let lista = [
+    { titulo: 'titulo ficticio 1', descricao: 'obra ficticia' },
+    { titulo: 'nome do novo produto', descricao: 'para que serve o novo produto' },
+    { titulo: 'JoJo Parte 5', descricao: 'Golden Wind, uma nova aventura de JoJo.' }
+];
 
-app.get('api/lista', (req,res)=> {
-    console.log(res);
-    res.jason(lista);//mandar como resposta a lista em formato jason
-})
+app.get('/api/lista', (req, res) => {
+    console.log(lista);
+    res.json(lista);
+});
 
-app.listen(3000, ()=> console.log('escutando'));
-app.use(express.static('site'));
+app.use(express.static('.'));
+
+app.listen(3000, () => console.log('Servidor escutando na porta 3000'));
